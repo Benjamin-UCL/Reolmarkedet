19a6f92b4d224913903d339391340a5b72f49ad5
Cecilie <ceciliek1989@outlook.com>
2025-09-12 13:21:16 +0200

    Add Rental model and Repository

---
+++ Database_Connection/Repository/RentalRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Database_Connection.Repository;
+
+public class RentalRepository : IRepository<Model.Rental>
+{
+    public void Add(Rental entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Rental> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Rental GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Rental entity)
+    {
+        throw new NotImplementedException();
+    }
+}
