52ef96d7d56a1989df9fdc56f0ed2d65b7147958
Benjamin Leonhardt <bjle71347@edu.ucl.dk>
2025-09-13 12:11:07 +0200

    typo fix

--- Database_Connection/Repository/SaleRepository.cs
+++ Database_Connection/Repository/SaleRepository.cs
@@ -12,25 +12,30 @@ public class SaleRepository : IRepository<Model.Sale>
     public void Add(Sale entity)
     {
         throw new NotImplementedException();
+        //string query = "INSERT INTO SALE (Id, Date, TotalAmount) VALUES (@Id, @Date, @TotalAmount)";
     }
 
     public void Delete(int id)
     {
         throw new NotImplementedException();
+        //string query = "DELETE FROM SALE WHERE Id = @Id";
     }
 
     public IEnumerable<Sale> GetAll()
     {
         throw new NotImplementedException();
+        //string query = "SELECT Id, Date, TotalAmount FROM SALE";
     }
 
     public Sale GetById(int id)
     {
         throw new NotImplementedException();
+        //string query = "SELECT Id, Date, TotalAmount FROM SALE WHERE Id = @Id";
     }
 
     public void Update(Sale entity)
     {
         throw new NotImplementedException();
+        //string query = "UPDATE SALE SET Date = @Date, TotalAmount = @TotalAmount WHERE Id = @Id";
     }
 }
