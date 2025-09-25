b692d7abca3fac7cd0d1735127f9ddb8d8a7ac2b
LauraPowerHansen <laha71386@edu.ucl.dk>
2025-09-25 10:43:50 +0200

    ItemViewModel oprettet mappe

---
+++ ReolMarkedTeam7/ViewModel/ItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReolMarkedTeam7.ViewModel;
+
+public class ItemViewModel : BaseViewModel
+{
+}
+
+//Constructor 
