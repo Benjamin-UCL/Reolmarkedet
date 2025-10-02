7a84092b356a1736bc66f9d6e1ae029f306b1cf7
Wuway <contactdieter@gmail.com>
2025-10-02 15:00:12 +0200

    implementering data validering email input tilføjet emailvalidationrule.cs i Utility

---
+++ GUI/Utility/EmailValidationRule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace GUI.Utility
+{
+    public class EmailValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string email = value as string;
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return new ValidationResult(false, "Email cannot be empty.");
+            }
+            // Simple regex for email validation
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            if (!Regex.IsMatch(email, pattern))
+            {
+                return new ValidationResult(false, "Invalid email format.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
